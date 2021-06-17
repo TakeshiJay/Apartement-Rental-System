caa3e048ffcfab5c46b983939be4d34b8e3aa470
Jacob Sunia <70661851+TakeshiJay@users.noreply.github.com>
2021-06-17 10:30:03 -0700

    updated format

--- Term_Project/JacobsRewriteOfCode/Expense.py
+++ Term_Project/JacobsRewriteOfCode/Expense.py
@@ -1,22 +1,25 @@
 from EIS import EIS
 
+
 class Expense:
     def __init__(self):
         eis = EIS.user_inp()
-        self.__month, self.__day, self.__category, self.__payee, self.__amount = eis.get_expense()
-    
+        self.__month, self.__day, self.__category, self.__payee, self.__amount = eis.get_expense(
+        )
+
     def get_month(self):
-        return(self.__month)
-    
+        return (self.__month)
+
     def get_day(self):
-        return(self.__day)
+        return (self.__day)
 
     def get_category(self):
-        return(self.__category)
-    
+        return (self.__category)
+
     def get_payee(self):
-        return(self.__payee)
-    
+        return (self.__payee)
+
     def get_amount(self):
-        return(self.__amount)
+        return (self.__amount)
+
 
