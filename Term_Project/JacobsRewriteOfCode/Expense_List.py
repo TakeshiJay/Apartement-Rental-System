c6f876f6ceeff7cba6ac753aa888e6ffeb299499
Jacob Sunia <70661851+TakeshiJay@users.noreply.github.com>
2021-06-17 10:29:36 -0700

    updated expense list with output. NOT FINAL

--- Term_Project/JacobsRewriteOfCode/Expense_List.py
+++ Term_Project/JacobsRewriteOfCode/Expense_List.py
@@ -4,9 +4,13 @@ class Expense_List:
         self.__updated_List = __Expense_List
 
     def add_nu_expense(self, month, day, category, payee, amount):
-        self.__Expense_List['Month'] = month 
+        self.__Expense_List['Month'] = month
         self.__Expense_List['Day'] = day
         self.__Expense_List['Category'] = category
         self.__Expense_List['Payee'] = payee
         self.__Expense_List['Amount'] = amount
-        self.__updated_List.append(self.__Expense_List) 
+        self.__updated_List.append(self.__Expense_List)
+
+    def print_expenses(self):
+        for i in self.__updated_List:
+            print(i['Month'],'/',i['Day'],i['Category'],i['Payee'],i['Amount'])
