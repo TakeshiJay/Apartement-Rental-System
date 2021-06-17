e0567728706571e4ce651fb491b33de340e1830d
Jacob Sunia <70661851+TakeshiJay@users.noreply.github.com>
2021-06-17 10:32:47 -0700

    Added a print statement if Apartment is taken

--- Term_Project/JacobsRewriteOfCode/Tenant_List.py
+++ Term_Project/JacobsRewriteOfCode/Tenant_List.py
@@ -1,11 +1,14 @@
 class Tenant_List:
+    
     def __init__(self, __tenant_list, __rent_list):
         self.__tenant_list = __tenant_list
         self.__tenant_pos = -1
-        self.__rental_list = [0]*12
+        self.__rental_list = [0] * 12
         self.__rent_list = __rent_list
 
     def add_nu_tenant(self, name, Apa_no):
-      if name not in self.__tenant_list:
-          self.__tenant_list[Apa_no] = name
-          self.__rent_list.append(self.__rental_list)
+        if Apa_no not in self.__tenant_list:
+            self.__tenant_list[Apa_no] = name
+            self.__rent_list.append(self.__rental_list)
+        else:
+            print('Apartment Taken, User Not Added')
