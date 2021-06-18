5fab344b2cc927a41b0dde358ba2f847917b7f92
Larry James <larryjdel@gmail.com>
2021-06-17 17:05:35 -0700

    larry's changes

--- Term_Project/JacobsRewriteOfCode/RentRecords.py
+++ Term_Project/JacobsRewriteOfCode/RentRecords.py
@@ -1,6 +1,27 @@
 class RentRecords:
-    def __init__(self, __Rent_List):
-        self.__Rent_List = __Rent_List
+    def __init__(self, __rentRecord):
+        self.__rentRecord = __rentRecord
 
     def insertRent(self, month, amount):
-        self.__Rent_List[month-1] = amount
+        self.__rentRecord[month-1] = amount
+
+    #Get Rent Total
+    def return_total_rent(self):
+        rTotal = 0
+        for key in __RentRecord:
+            rTotal += __rentRecord[key]
+        return rTotal
+    
+    #Print Rent Record
+    def display(self):
+        print("==== Rent Summary =====")
+        print(self)
+        #Tabulate
+        '''
+        headers = self.__Rent_List.keys()
+        rows = [x.values() for x in self.__updatedList]
+        print(tabulate.tabulate(rows, headers, tablefmt='rst'))
+        '''
+        #Print Rent Total Last
+        print("\nRent Total: " + str(return_total_rent()))
+        
