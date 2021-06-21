ef2d1283ba53128f7cfa5b9eac37306dd6c1fca8
Jacob Sunia <70661851+TakeshiJay@users.noreply.github.com>
2021-06-21 08:52:57 -0700

    fixed RentInput Screen to look like class diag

--- Term_Project/JacobsRewriteOfCode/RentInputScreen.py
+++ Term_Project/JacobsRewriteOfCode/RentInputScreen.py
@@ -12,22 +12,56 @@
 # Professor Phuong Nguyen          #
 ####################################
 """
-
+from RentRow import RentRow
+from RentRecords import RentRecords
 
 class RentInputScreen:  # rental input screen
-    def __init__(self, Name, month, amount):
-        self.__Name = Name
-        self.__month = month
-        self.__amount = amount
+    def __init__(self, rent_list, tenant_list):
+        self.__Name = None
+        self.__month = -1
+        self.__idx = -1
+        self.__amount = 0
+        self.__rent_list = rent_list
+        self.__tenant_list = tenant_list
 
     def getRent(self):
         return(self.__Name, self.__month, self.__amount)
 
-    @classmethod
-    def inputRentPayment(cls):
-        return(cls(
-            input('Enter Tenant Name: '),
-            int(input('Enter Month No.: ')),
-            float(input('Enter Rent Payment: '))
-            )
-        )
+    def getIndex(self, login_idx):
+        for i, dic in enumerate(self.__tenant_list[login_idx]):
+            if dic['name'] == self.__Name:
+                self.__idx = i
+
+    def inputRent(self, login_idx):
+        flag = False
+        self.__Name = input('Enter a Tenant Name to Add Payment: ')
+        x = 0
+
+        for i in self.__tenant_list[login_idx]:
+            if self.__Name in i['name']:
+                x = 1
+                break
+        if x == 0:
+            self.inputRent(login_idx)
+        self.getIndex(login_idx)
+        
+        while (self.__month < 0) or (self.__month > 13):
+            try:
+                self.__month = int(input('Please Select a Month Number:'))
+                self.__month = self.__month - 1
+            except:
+                print('Invalid Entry, Try Again...')
+        while flag == False:
+            self.__amount = float(input('Please Enter an Amount you want to Add:'))
+            print('Amount:',self.__amount)
+            ays = input('Is this the Correct amount (Y/N):')
+            if ays.lower() == 'y':
+                flag = True
+        rentRow = RentRow(self.__Name, self.__month, self.__amount)
+        rentRecord = RentRecords(self.__rent_list, self.__tenant_list)
+        rentRecord.insertRent(rentRow, login_idx, self.__idx)
+
+        return(self.__rent_list)
+
+        
+
