4b08d27e1a50e4585eb0555bd49ec7413bef4342
Jacob Sunia <70661851+TakeshiJay@users.noreply.github.com>
2021-06-21 08:54:00 -0700

    updated RentRecords to insert rent
    *** Currently Working on Display ***

--- Term_Project/JacobsRewriteOfCode/RentRecords.py
+++ Term_Project/JacobsRewriteOfCode/RentRecords.py
@@ -1,3 +1,4 @@
+import tabulate
 # -*- coding: utf-8 -*-
 """
 ########## Term Project ############
@@ -13,23 +14,18 @@
 # Professor Phuong Nguyen          #
 ####################################
 """
-
+from RentRow import RentRow 
 
 class RentRecords:
-    def __init__(self, __rentRecord):
+    def __init__(self, __rentRecord, __tenantList):
         self.__rows = __rentRecord  # RentRecords dictionary
+        self.__tenantList = __tenantList
+        self.__months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+        
+    def insertRent(self, rent_record, user_login_idx, ten_idx):
+        name, month, amount = rent_record.getInfo()
+        self.__rows[user_login_idx][ten_idx][month] = amount
 
-    def insertRent(self, month, amount):
-        if month > 0 and month < 13:
-            self.__rows[month - 1] = amount
-            return True
-        else:
-            print(f"RentRecords.insertRent(): invalid month {month}")
-            return False
-
-    def insertRentRow(self, row):
-        print("RentRecords.insertRentRow() not implemented.")
-        return False
 
     # Get Rent Total
     def getSumOfRents(self):
@@ -47,13 +43,29 @@ class RentRecords:
         return rTotal
 
     # Print Rent Record
-    def display(self):
+    def display(self, user_login_idx):
         print("\n==== Rent Summary =====")
         # Tabulate
-        '''
-        headers = self.__Rent_List.keys()
-        rows = [x.values() for x in self.__updatedList]
-        print(tabulate.tabulate(rows, headers, tablefmt='rst'))
-        '''
+        for i in self.__months:
+            print(i, end='\t\t')
+        print()
+
+        # for i in self.__rows[user_login_idx]:
+        #     for j in i:
+        #         aligned_j = "{:<5}".format(j)
+        #         print(aligned_j, end='\t')
+        #     print()
+        # print()
+        for i in range(len(self.__rows[user_login_idx])):
+            print(self.__rows[user_login_idx][i])
+
+        # headers = self.__rows[user_login_idx].keys()
+        # rows = [x.values() for x in self.__rows[user_login_idx]]
+        # print(tabulate.tabulate(rows, headers, tablefmt='rst'))
+
+        # headers = self.__rows.keys()
+        # rows = [x.values() for x in self.__rows]
+        # print(tabulate.tabulate(rows, headers, tablefmt='rst'))
+        
         # Print Rent Total Last
-        print(f"\nRent Total: ${self.getSumOfRents():.2f}")
+        # print(f"\nRent Total: ${self.getSumOfRents():.2f}")
