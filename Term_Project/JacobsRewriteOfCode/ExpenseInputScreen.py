42c6ae58e92e50892af3b099dbba4f252b5fdcb8
Jacob Sunia <70661851+TakeshiJay@users.noreply.github.com>
2021-06-21 21:37:26 -0700

    fixed input screen to match design

--- Term_Project/JacobsRewriteOfCode/ExpenseInputScreen.py
+++ Term_Project/JacobsRewriteOfCode/ExpenseInputScreen.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 ########## Term Project ############
@@ -12,26 +13,49 @@
 # Professor Phuong Nguyen          #
 ####################################
 """
-
+from Expense import Expense
+from ExpenseRecords import ExpenseRecords
 
 class ExpenseInputScreen:  # Expense input screen
-    def __init__(self, month, day, category, payee, amount):
-        self.__month = month
-        self.__day = day
-        self.__category = category
-        self.__payee = payee
-        self.__amount = amount
+    def __init__(self, expense_list):
+        self.__month = -1
+        self.__day = -1
+        self.__category = None
+        self.__payee = None
+        self.__amount = None
+        self.__expense_list = expense_list
 
     def getExpense(self):
         return(self.__month, self.__day, self.__category,
                self.__payee, self.__amount)
 
-    @classmethod
-    def inputExpense(cls):
-        return cls(
-            int(input("Month No.: ")),
-            int(input("Day No.: ")),
-            input("Category: "),
-            input("Payee: "), 
-            float(input("Amount: "))
-        )
+    def inputExpense(self):
+        hiddenMonth = 0
+      
+        while (self.__month < 0) or (self.__month > 11):
+            try:
+                self.__month = int(input('Please Enter a Month Value (1-12):'))
+                hiddenMonth = self.__month
+                self.__month -= 1
+            except:
+                print('Invalid Input, Try again...')
+
+        while (self.__day < 0) or (self.__day > 29):
+            try:
+                if (hiddenMonth == 2):
+                    self.__day = int(input('Please Enter a Day Value(1-28):'))
+                    self.__day -= 1
+                    if(self.__day > 27):
+                        self.__day = -1
+                else:
+                    self.__day = int(input('Please Enter a Day Value(1-31):'))
+                    self.__day -= 1
+            except:
+                print('Invalid Input, Please Try Again')
+                self.__day = -1
+        self.__category = input('Enter Expense Category(Repairing, Utilities):')
+        self.__payee = input('Enter payee (Bob\'s Hardware, Big Electric Co):')
+        self.__amount = float(input('Enter amount(39.95):'))
+        expense = Expense(self.__month, self.__day, self.__category, self.__payee, self.__amount)
+        expenseRecord = ExpenseRecords(self.__expense_list)
+        expenseRecord.insertExp(expense)
