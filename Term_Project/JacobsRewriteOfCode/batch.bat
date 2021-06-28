d20b34259f16342f2fa9374a06cb7f004d9c11f9
Jacob Sunia <70661851+TakeshiJay@users.noreply.github.com>
2021-06-27 17:32:03 -0700

    Update batch.bat

--- Term_Project/JacobsRewriteOfCode/batch.bat
+++ Term_Project/JacobsRewriteOfCode/batch.bat
@@ -4,6 +4,7 @@ print    answer y
 .\python-3.8.5-amd64
 print 2. Installing the tabulate tabulation module
 pip install tabulate
+cls
 print 3. Launching Apartment Rental System 1.0 by Team 6
 python.exe .\main.py
 pause
