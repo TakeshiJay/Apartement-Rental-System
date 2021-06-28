2861927292f5c51640d11d540054d36f07aad2ea
Sterling Engle <sjengle@ucla.edu>
2021-06-27 17:09:22 -0700

    added auto install of python-3.8.5 it needs cls

--- Term_Project/JacobsRewriteOfCode/batch.bat
+++ Term_Project/JacobsRewriteOfCode/batch.bat
@@ -1,5 +1,9 @@
-# Apartment Rental System 1.0 by Team 6 deployment script
-print Launching Apartment Rental System 1.0 by Team 6
-cls
+print Apartment Rental System 1.0 by Team 6 deployment script
+print 1. Launching Python 3.8.5 Installation by Python.org
+print    answer y
+.\python-3.8.5-amd64
+print 2. Installing the tabulate tabulation module
+pip install tabulate
+print 3. Launching Apartment Rental System 1.0 by Team 6
 python.exe .\main.py
 pause
