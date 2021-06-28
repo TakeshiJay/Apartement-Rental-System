94013d3485ef5c48ff259584d4fec1da13e4fca1
Sterling Engle <sjengle@ucla.edu>
2021-06-28 13:12:25 -0700

    renamed

---
+++ Term_Project/ARS_Multiuser/ARS.bat
@@ -0,0 +1,10 @@
+print Apartment Rental System Multiuser Edition 2.0.1 by Team 6 Windows 10 Deployment Script
+print 1. Launching Python 3.8.5 Installation by Python.org
+print    answer y
+.\python-3.8.5-amd64
+print 2. Installing the tabulate tabulation module
+pip install tabulate
+cls
+print 3. Launching Apartment Rental System Multiuser Edition 2.0.1 by Team 6
+python.exe .\main.py
+pause
